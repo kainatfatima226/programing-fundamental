fd512f07954e37582c6f4af1cd694b544d4d0112
kainatfatima226 <kainatfatima2225@gmail.com>
2024-10-14 11:43:22 -0700

    Add files via upload

---
+++ task 1 pf.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num1,num2;
+cout<<"enter two numbers:"<<endl;
+cin>>num1>>num2;
+int sum=num1+num2;
+cout<<"sum:"<<sum<<endl;
+int mul=num1*num2;
+cout<<"multiplication:"<<mul<<endl;
+int sub=num1-num2;
+cout<<"subtraction:"<<sub<<endl;
+float div=num1/num2;
+cout<<"division:"<<div<<endl;
+
+
+return 0;
+
+}
